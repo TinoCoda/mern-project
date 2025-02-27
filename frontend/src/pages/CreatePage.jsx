686203628316c8b36a6689c8c28acbb7996481e1
Destin B.Moanda <moanda@live.fr>
2025-02-27 06:46:47 +0100

    set the main structure of the frontend

---
+++ frontend/src/pages/CreatePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button } from "@chakra-ui/react"
+
+const CreatePage = () => {
+  return (
+    <>
+    <div>CreatePage</div>
+     <Button >Hello </Button> 
+    </>
+
+  )
+}
+
+export default CreatePage
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const HomePage = () => {
+  console.log("HomePage loaded!") 
+  return (
+    <div>HomePage</div>
+  )
+}
+
+export default HomePage
